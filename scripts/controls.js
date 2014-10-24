1a51bcb54f7bdd7f8bbad54c4f320a0d4f19f579
cdisciascio <cecidisi@gmail.com>
2014-10-24 13:48:26 +0200

    generic url to download results

--- scripts/controls.js
+++ scripts/controls.js
@@ -8,19 +8,17 @@
 
 
     var downloadResults = function(e){
-        console.log("download");
+        var scriptURL = (document.URL).replace('controls.html', 'download.php');
         var d = new Date();
         var timestamp = d.getFullYear() + '-' + (parseInt(d.getMonth()) + 1) + '-' + d.getDate() + '_' + d.getHours() + '.' + d.getMinutes() + '.' + d.getSeconds();
 
         $.generateFile({
             filename	: 'evaluation_results_' + timestamp + '.txt',
             content		: JSON.stringify(taskStorage.getEvaluationResults()),
-            script		: 'http://localhost:8888/uRank/download.php'
+            script		: scriptURL
         });
         e.preventDefault();
-        console.log("nada");
     };
-
 
 
     (function init(){
