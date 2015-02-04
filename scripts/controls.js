47d4ea62c7bfebdc06ffb4602a32137b9759581e
cdisciascio <cdisciascio@know-center.at>
2015-02-04 16:51:41 +0100

    evaluation results succesfully inserted in DB

--- scripts/controls.js
+++ scripts/controls.js
@@ -21,6 +21,7 @@
             timestamp = d.getFullYear() + '-' + (parseInt(d.getMonth()) + 1) + '-' + 
                 d.getDate() + '_' + d.getHours() + '.' + d.getMinutes() + '.' + d.getSeconds();
 
+        console.log(scriptURL);
         $.generateFile({
             filename	: 'evaluation_results_' + timestamp + '.txt',
             content		: JSON.stringify(taskStorage.getEvaluationResults()),
@@ -40,13 +41,15 @@
                 { query: 'circular economy', keywords: ['management&waste', 'china&industrial&symbiosis', 'circular&economy&fossil&fuel&system&waste']}];
 
         taskStorage.getEvaluationResults().forEach(function(result){
+            var user_num = (result.user.toString().length == 1) ? '0'+result.user.toString() : result.user.toString();
+            
             result["tasks-results"].forEach(function(task) {
                 if(task["tool-aided"] === 'yes') {
                     task["questions-results"].forEach(function(question) {
                         var kw = getKeywords(task.query, question["question-number"], kw_aux);
                         question["selected-items"].forEach(function(item) {
                             linked_user_doc_qk.push({
-                                user_id: result.user,
+                                user_id: 'user_' + user_num,
                                 document_id: item.id,
                                 document_title: item.title,
                                 query: task.query,
@@ -57,14 +60,35 @@
                 }
             });
         });
-        console.log('linked_user_doc_qk');
-        console.log(linked_user_doc_qk);
+       // console.log('linked_user_doc_qk');
+        //console.log(linked_user_doc_qk);
 
         function getKeywords(query, questionNumber, kw_aux) {
             var index = kw_aux.getIndexOf(query, 'query');
             return kw_aux[index].keywords[questionNumber - 1].split('&');
         }
-
+        
+        var host = "http://localhost/RankingViz/server/recommender.php";
+        var successfulInsertions = 0;
+        
+        linked_user_doc_qk.forEach(function(link, i) {
+        
+            var dataToSend = { action: 'initial_data_added', data: link };
+            // Call server
+            $.post(host, dataToSend)
+            .done(function(reqData){
+                //console.log(JSON.parse(reqData));
+                successfulInsertions++;
+            })
+            .fail(function(jqXHR, textStatus) {
+                console.log(jqXHR.responseJSON.message);
+            });
+        });
+        
+        setTimeout(function() {
+            console.log('Total links = ' + linked_user_doc_qk.length + ' --- Successful insertions = ' + successfulInsertions);
+        }, 5000);
+    
     };
 
 
---
+++ server/recommender.php
@@ -0,0 +1,208 @@
+<?php
+
+
+if(empty($_POST['action']) || empty($_POST['data'])){
+    return_error('A POST parameter is not set', '1001');
+}
+
+$action = $_POST['action'];
+$data = $_POST['data'];
+$response = '';
+
+
+switch ($action) {
+    case 'bookmark_added':
+    $response = insert_bookmark($data);
+        if($response == 'Success')
+            $response = get_recommendations_for_bookmarked_document($data);
+        break;        
+    case 'bookmark_deleted':
+        $response = delete_bookmark($data); break;
+    case 'keywords_changed':
+        $response = get_recommendations_for_keywords($data); break;
+    case 'initial_data_added':
+        $response = insert_bookmark($data); break;
+    default: 
+        return_error('Unknown action', '1002'); break;
+}
+
+echo $response;
+
+
+
+function insert_bookmark($parameters) {
+
+    $expected_parameters = array('user_id', 'document_id', 'document_title', 'query', 'keywords');    
+    validate_all_parameters_set($parameters, $expected_parameters);  // if false returns echoes error message back
+    $qk_string = get_qk_string($parameters['query'], $parameters['keywords']);
+    
+    $mysqli = get_mysqli();
+    set_sp_parameter($mysqli, '@user_id', $parameters['user_id']);
+    set_sp_parameter($mysqli, '@document_id', $parameters['document_id']);
+    set_sp_parameter($mysqli, '@document_title', convert_smart_quotes($parameters['document_title']));
+    set_sp_parameter($mysqli, '@qk_string', $qk_string);
+    $result = call_sp($mysqli, 'insert_data', '@user_id, @document_id, @document_title, @qk_string');
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row['message'];
+}
+
+
+
+function delete_bookmark($parameters) {
+
+    $expected_parameters = array('user_id', 'document_id', 'query', 'keywords');    
+    validate_all_parameters_set($parameters, $expected_parameters);  // if false returns echoes error message back
+    $qk_string = get_qk_string($parameters['query'], $parameters['keywords']);
+
+    $mysqli = get_mysqli();
+    set_sp_parameter($mysqli, '@user_id', $parameters['user_id']);
+    set_sp_parameter($mysqli, '@document_id', $parameters['document_id']);
+    set_sp_parameter($mysqli, '@qk_string', $qk_string);
+    $result = call_sp($mysqli, 'delete_link', '@user_id, @document_id, @qk_string');
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row['message'];
+}
+
+
+
+function get_recommendations_for_bookmarked_document($parameters) {
+    
+    
+    
+}
+
+
+function get_recommendations_for_keywords($parameters) {
+
+    $user_id = $parameters['user_id'];
+    $document_id = $parameters['document_id'];
+    $query = $parameters['query'];
+    $keywords = $parameters['keywords'];
+
+
+    if(!isset($user_id) || !isset($document_id) || !isset($document_title) || !isset($query) || !isset($keywords)) {
+        return_error('A parameter in data is not set', '1003');
+    }
+
+    if(!is_array($keywords)) {
+        return_error('Keywords parameter is not an array', '1004');
+    }
+
+    sort($keywords);
+    $qk_string = $query;
+    foreach($keywords as $keyword)
+        $qk_string = $qk_string.'&'.$keyword;
+    $mysqli = get_mysqli();
+    $result = $mysqli->query("CALL insert_data($user_id, $document_id, $document_title, $qk_string)");
+
+    while($row = $result->fetch_assoc()) {
+        $obj = new stdClass();
+        $obj->id = $row['id'];
+        $obj->title = $row['title'];
+        $res[] = $obj;
+    }
+  
+    $mysqli->close();
+    return json_encode($res);
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function validate_all_parameters_set($parameters, $expected_parameters) {
+
+    foreach($expected_parameters as $p_key) {
+        if(!isset($parameters[$p_key]))
+            return_error("Parameter '".$p_key."' is not set");   
+    }
+}
+    
+
+function get_qk_string($query, $keywords) {
+    if(!is_array($keywords)) {
+        return_error('Keywords parameter is not an array', '1004');
+    }
+
+    sort($keywords);
+    $qk_string = $query;
+    foreach($keywords as $keyword)
+        $qk_string = $qk_string.'&'.$keyword;
+    
+    return $qk_string;
+}
+
+
+function convert_smart_quotes($string) 
+{ 
+    $search = array(chr(145), 
+                    chr(146), 
+                    chr(147), 
+                    chr(148), 
+                    chr(151)); 
+
+    $replace = array("'", 
+                     "'", 
+                     '"', 
+                     '"', 
+                     '-'); 
+
+    return str_replace($search, $replace, $string); 
+}
+
+
+function get_mysqli() {
+    // connect to db
+    $mysqli = new mysqli("localhost", "urank", "urank", "urank-schema");
+
+    // Check connection
+    if ($mysqli->connect_error) {
+        die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+    }
+    return $mysqli;
+}
+
+
+function set_sp_parameter($mysqli, $p_name, $p_value) {    
+  
+//    $aa = str_split($p_value);
+//    $s = '';
+//    foreach($aa as $a) {
+//        if(ord($a) == 39)
+//            $a = '\'';
+//        $s = $s.$a;
+//    }
+//    $p_value = $s;
+//    echo $s;
+    
+    $p_value = addslashes($p_value);
+    
+    if (!$mysqli->query("SET ".$p_name." = '".$p_value."'")) {
+    
+        return_error("SET ".$p_name." = ".$p_value." failed: (" . $mysqli->errno . ") ", $mysqli->error);
+        //return_error($p_value." : " .$s, $mysqli->error);
+    }
+}
+
+
+function call_sp($mysqli, $sp_name, $parameter_string) {
+    if (! $result = $mysqli->query("CALL ".$sp_name."(".$parameter_string.")")) {
+        return_error("CALL ".$sp_name."() failed: (" . $mysqli->errno . ") \n ", $mysqli->error);
+    }
+    return $result;
+}
+
+
+
+function return_error($message, $code) {
+    header('HTTP/1.1 500 Internal Server Error');
+    header('Content-Type: application/json; charset=UTF-8');
+    die(json_encode(array('message' => 'ERROR '.$message, 'code' => $code)));
+    echo 'ERROR -- '.$message;
+}
+
+
+
+?>
